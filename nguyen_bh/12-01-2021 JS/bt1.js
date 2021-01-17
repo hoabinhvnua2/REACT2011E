ef3e3655a48489ea072e95daa303630a1e6968a6
buihoangnguyenhb <buihoangnguyenhb@gmail.com>
2021-01-17 18:57:57 +0700

    em gui bt flex,grid, js-buoi 1 ak

---
+++ nguyen_bh/12-01-2021 JS/bt1.js
@@ -0,0 +1,43 @@
+// tính diện tích hình tam giác.
+var a = 10;
+var h = 20;
+
+/**
+ * a: cạnh đáy của 1 hình tam giác
+ * h: chiều cao của tam giác đó
+ * Yêu cầu: khai báo biến s bằng diện tích của hình tam giác này 
+ */
+var canhDay=3;
+var chieuCao=5;
+var s=a*h/2;
+console.log('dien tich tam giac la:',s);
+
+// tinh diện tích hình chữ nhật.
+
+var width = 10;
+var height = 20;
+
+var s = width * height;
+/**
+ * width: Chiều rộng của 1 hình chữ nhật
+ * height: Chiều cao của hình chữ nhật
+ * s: diện tích của hình chữ nhật (code mẫu)
+ * Yêu cầu: 
+ * - Khai báo biến p là chu vi của hình chữ nhật này.
+ */
+var p= (width+height)*2;
+console.log("chu vi HCN la:",p)
+
+// tính diện tích hình tròn
+ 
+var r = 10;
+
+var s = r * r * 3.14;
+/**
+ * r: Bán kính của 1 hình tròn
+ * s: Diện tích của hình tròn đó
+ * Yêu cầu: 
+ * - Khai báo biến p là chu vi của hình tròn này.
+ */
+var p = 2*3.14*r;
+console.log("chu vi cua hinh tron la",p);
--- nguyen_bh/readme.txt
+++ nguyen_bh/readme.txt
@@ -1,2 +1,5 @@
 Em đã tìm hiểu được HTML cơ bản một số cái nâng cao ở HTML 5
  thì em chưa biết.
+ Ngày 17/01/2021 : ở bài tập flex có vấn đề là 2 cái cột ở margin nó bị lệch nhau thì em vẫn chưa hiểu rõ được nguyên nhân của nó là gì anh giải thích cho em với ạ.~~~
+ Ngày 17/01/2021: bài tập grid nay em làm vội vẫn còn 1 ít lỗi ở phần menu em sẽ updata lại code sớm ạ.
+ 
