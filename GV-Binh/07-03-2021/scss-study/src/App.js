88df356aefbefe75a6c8b6d67d09983adf4a3ddc
= <=>
2021-03-07 23:00:54 +0700

    update day 15

---
+++ GV-Binh/07-03-2021/scss-study/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="header -background">
+        <p className="header__title -font-size">
+          Xin chào các bạn!
+        </p>
+        <p className="header__content">Xin chào các bạn!</p>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
